93ae8700c189c5aa7fad43c352b75e7de87b51cb
Alvin Mathew <alvinmathewp80@gmail.com>
2025-01-04 11:47:49 +0530

    add: array

--- arrysum.cpp
+++ arrysum.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int size, sum = 0;
-    cout << "enter the size of the array :";
-    cin >> size;
-    int arr[size];
-    cout << "enter " << size << " elements the the array :" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        cin >> arr[i];
-        sum += arr[i];
+class array{
+    private:
+    int arr[100];
+    int size;
+public:
+void inputarray();
+void countoddeven();
+};
+void array::inputarray(){
+    cout<<"Enter size of the array:";
+    cin>>size;
+    cout<<"Enter the elements of array:";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
     }
-    cout << "the sum of the elements the array is :" << sum << endl;
-    return 0;
+}
+void array::coutoddeven(){
+    int oddcount=0,evencount=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]%2==0)
+        evencount++
+    else
+    oddcount++;
+            }
+cout<<"No of even number :"<<evencount<<endl;
+cout<<"no of odd numbers: "<<oddcount<<endl;
+}
+void main(){
+    
+
 }
