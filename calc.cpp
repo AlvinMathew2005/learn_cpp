8fde8a91983a85db0613e63be85f2bcd29ae1683
Alvin Mathew <alvinmathewp80@gmail.com>
2025-01-04 11:49:27 +0530

    add: smallest using nested if

--- calc.cpp
+++ calc.cpp
@@ -1,1 +1,27 @@
+
 #include <iostream>
+using namespace std;
+ int main(){
+    int a,b,c,d;
+    cout <<"Enter four numbers:";
+    cin>>a>>b>>c>>d;
+    if(a<b&&a<c){
+        if(a<d){
+        cout<<"A is smallest :"<<a<<endl;
+        }
+    }
+    if(b<a&&b<c){
+        if(b<d){
+            cout<<"B is smallest: "<<b<<endl;
+        }
+    }
+    if (c<a&&c<b){
+        if(c<d){
+        cout<<"C is smallest: "<<c<<endl;
+    }
+    else{
+        cout<<"D is smallest:"<<d<<endl;
+    }
+ }
+ return 0;
+ }
